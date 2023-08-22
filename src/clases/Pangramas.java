c2ee810ea48b9abeec53994d630a80e1d657132d
ProgramadorJava1996 <programadorjava99@gmail.com>
2023-08-22 11:15:29 -0500

    Reestructuracion en paquetes

--- src/clases/Pangramas.java
+++ src/clases/Pangramas.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package mecanografia;
+package clases;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
